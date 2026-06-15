dc980b4713fb11a00769468c635eae7347d2648d
root <root@WIN-KI5DH3B5EM5.localdomain>
2026-06-16 03:45:32 +0400

    changed dropout2d to dropout

--- models.py
+++ models.py
@@ -82,7 +82,7 @@ class DeepCNN(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Dropout2d(0.2),
+            nn.Dropout(0.2),
 
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
@@ -91,13 +91,13 @@ class DeepCNN(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Dropout2d(0.2),
+            nn.Dropout(0.2),
 
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Dropout2d(0.2),
+            nn.Dropout(0.2),
 
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
